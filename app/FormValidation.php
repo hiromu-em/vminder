95a7c913dacd7ca7e97eaa3d8bc4017bfa438dc8
hiromushinjo <v.versehub@gmail.com>
2026-03-10 19:29:42 +0900

    refactor: 名前空間を変更

--- app/FormValidation.php
+++ app/FormValidation.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace Vmatch;
+namespace Vminder;
 
-use Vmatch\Result;
+use Vminder\Result;
 
 class FormValidation
 {
--- app/GoogleOauth.php
+++ app/GoogleOauth.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Vmatch;
+namespace Vminder;
 
 use Google\Client;
 
